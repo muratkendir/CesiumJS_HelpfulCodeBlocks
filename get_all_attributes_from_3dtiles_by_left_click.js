11e491d1aa203b00cf946cbed61ef3e8c79682a0
Murat Kendir <muratkendir@gmail.com>
2019-10-15 11:16:35 +0300

    Create get_all_attributes_from_3dtiles_by_left_click.js

---
+++ get_all_attributes_from_3dtiles_by_left_click.js
@@ -0,0 +1,32 @@
+var viewer = new Cesium.Viewer('cesiumContainer', {
+    terrainProvider: Cesium.createWorldTerrain()
+});
+
+var tileset = viewer.scene.primitives.add(
+    new Cesium.Cesium3DTileset({
+        url: Cesium.IonResource.fromAssetId(48447)
+    })
+);
+
+viewer.zoomTo(tileset)
+    .otherwise(function (error) {
+        console.log(error);
+    });
+
+var scene = viewer.scene;
+if (!scene.pickPositionSupported) {
+    console.log('This browser does not support pickPosition.');
+}
+
+var handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
+handler.setInputAction(function(result) {
+    var feature = viewer.scene.pick(result.position);
+    if (feature instanceof Cesium.Cesium3DTileFeature) {
+        var propertyNames = feature.getPropertyNames();
+        var length = propertyNames.length;
+        for (var i = 0; i < length; ++i) {
+            var propertyName = propertyNames[i];
+            console.log(propertyName + ': ' + feature.getProperty(propertyName));
+        }
+    }
+}, Cesium.ScreenSpaceEventType.LEFT_CLICK);
